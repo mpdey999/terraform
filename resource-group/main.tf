4f53c286d68f1529d1196f9d9fb3b778f328108c
mpdey999 <mpdey999@gmail.com>
2026-09-12 19:26:58 +0530

    Parameterize resource group name in main.tf

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -13,7 +13,12 @@ provider "azurerm" {
   features {}
 }
 
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "mukti-terraform-rg"
+  name     = var.resource_group_name
   location = "East US"
 }

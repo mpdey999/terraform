44054cf45292829f73795eb1c2b43f120f2724c4
Mukti De <mpdey999@gmail.com>
2026-09-12 18:27:50 +0530

    Add Terraform resource group

---
+++ resource-group/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "mukti-terraform-rg"
+  location = "East US"
+}

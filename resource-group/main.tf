bb8db8f769b3e9684ffa672fc7675b9c12112912
Mukti De <mpdey999@gmail.com>
2026-09-13 22:59:51 +0530

    Fix Terraform configuration

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = "centralindia"
@@ -9,25 +8,3 @@ resource "azurerm_resource_group" "this" {
     project    = "Contact_Registration"
   }
 }
-=======
-module "delegate" {
-  source = "harness/harness-delegate/kubernetes"
-  version = "0.2.3"
-
-  account_id = "PpAShYwORdCYNK9nLAQ4qw"
-  delegate_token = "MDJmMzdkNjE5ODY4MzEwZWYzYmQ0Mzc3N2Q2Y2MzNmE="
-  delegate_name = "terraform-delegate"
-  deploy_mode = "KUBERNETES"
-  namespace = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io"
-  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:26.08.89804"
-  replicas = 1
-  upgrader_enabled = true
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
->>>>>>> 19a8cf48ed7de1ac0dca0a8a73dd8742df2614ff

6447217347a030aca4a09cc0d750fdf4c8a31074
Mukti De <mpdey999@gmail.com>
2026-09-14 22:44:47 +0530

    test

--- resource-group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  type        = string
-
-  validation {
-    condition     = length(trimspace(var.resource_group_name)) >= 3
-    error_message = "Resource group name must contain at least 3 characters."
-  }
-}
